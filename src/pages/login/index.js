c538c94bb20422628aa5e57c7fa1bc0f288e1932
arman-g027 <arman.grigoryan96.ag@gmail.com>
2024-10-07 20:02:07 +0400

    add pagination for login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,23 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Login = () => {
 
+    const [page, setPage] = useState(1);
+    const [resultCount, setResultCount] = useState(10);
+    const [showModal, setShowModal] = useState(false) ;
 
-    const [count, setCount] = useState(0);
+    useEffect(() => {
+        fetch(`https://randomuser.me/api/?page=${page}&results=10&seed=abc`)
+            .then((resp) => {
+                return resp.json();
+            }).then((data) => {
+                console.log(data);
+            })
+    }, [page]);
 
-    const handleInctrementCount = () => {
-        setCount((prevState) => {
-            return prevState + 1;
-        });
-
-        console.log(count);
-    }
+    const handleChangePagination = value => {
+        setPage(value === 'next' ? page + 1 : page - 1)
+    };
 
     return (
         <div>
-            <button onClick={handleInctrementCount}>{count}</button>
-        </div>
+            <button onClick={() => handleChangePagination('prev')}>
+                prev
+            </button>
+
+            <span>page={page}</span>
+
+            <button onClick={() => handleChangePagination('next')}>
+                next
+            </button>
+
+            <button onClick={() => setShowModal(!showModal)}>
+                {showModal ? 'Close Modal' : 'Open Modal'}
+            </button>
+
+            {
+                showModal && (
+                    <div>
+                        <h2>Modal</h2>
+                    </div>
+                )
+            }
+        </div >
     )
-}
+};
 
 export default Login;
